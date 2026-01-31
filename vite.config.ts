f63524dd1f1cfc9df1656cbefe6e712b5da9d2f5
Sergei Chevdar <serj.elessar@gmail.com>
2026-01-31 15:41:21 +0300

    chore: add Vite configuration for project build and development setup

---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import { globSync } from 'glob';
+import { parse,relative, resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+	publicDir: resolve(__dirname, 'public'),
+	plugins: [
+		createHtmlPlugin({
+			minify: false
+		})
+	],
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				'scripts/main': resolve(__dirname, 'src/scripts/main.ts'),
+				'styles/main': resolve(__dirname, 'src/styles/main.css'),
+				...Object.fromEntries(
+					globSync('src/{scripts,styles}/views/**/page.{ts,css}').map(file => {
+						const { dir } = parse(file);
+						// Remove 'src/' prefix and don't include '/page' in the key
+						// e.g., 'scripts/views/home'
+						const relPath = relative('src', dir);
+						
+						return [
+							// Use dir path without '/page'
+							relPath,
+							resolve(__dirname, file)
+						];
+					})
+				)
+			},
+			output: {
+				entryFileNames: 'assets/[name].js',
+				assetFileNames: 'assets/[name].[ext]',
+				chunkFileNames: 'assets/chunks/[name]-[hash].js',
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+			'@scripts': resolve(__dirname, 'src/scripts'),
+			'@styles': resolve(__dirname, 'src/styles')
+		}
+	},
+	server: {
+		strictPort: true,
+		host: '0.0.0.0',
+		port: 3000
+	}
+});
