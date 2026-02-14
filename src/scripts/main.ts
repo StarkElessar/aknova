2c1d833d7e03428f47e9456d711d1a25054d9a54
Sergei Chevdar <serj.elessar@gmail.com>
2026-02-14 17:29:53 +0300

    feat(lazy-load): implement lazy loading for images and iframes across multiple pages

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,16 +1,18 @@
 // Global JavaScript code that runs on all pages
 import { initVarScrollbarWidth } from '@scripts/shared/lib/init-var-scrollbar-width';
 import { initPopups } from '@scripts/shared/lib/popup';
+import { LazyLoadManager } from '@scripts/shared/lib/lazy-load-manager';
+import { formHandler } from '@scripts/shared/lib/form-handler';
 
 import { burgerMenuInit } from '@scripts/features/burger-menu-init';
 import { initGoTopButton } from '@scripts/features/go-top-button';
 import { initPopupTitleHandler } from '@scripts/features/popup-title-handler';
-import { formHandler } from '@scripts/shared/lib/form-handler';
 import { initMaskInput } from '@scripts/features/mask-input.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
 	initVarScrollbarWidth();
 	burgerMenuInit();
+	LazyLoadManager.getInstance();
 	initGoTopButton();
 	initPopups();
 	initPopupTitleHandler();
---
+++ src/scripts/shared/lib/lazy-load-manager.ts
@@ -0,0 +1,15 @@
+import LazyLoad from 'vanilla-lazyload';
+import type { ILazyLoadInstance, ILazyLoadOptions } from 'vanilla-lazyload';
+
+export class LazyLoadManager {
+	private static instance: ILazyLoadInstance;
+
+	static getInstance(options: Partial<ILazyLoadOptions> = {}) {
+		return LazyLoadManager.instance ??= new LazyLoad({
+			elements_selector: '.lazy',
+			threshold: 500,
+			use_native: false,
+			...options
+		});
+	}
+}
